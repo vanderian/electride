e6df8d2975b0a6c5fc98ac1f168a6bedc58ba74b
Marian Vanderka <vanderka.marian@gmail.com>
2017-10-04 10:39:54 +0200

    crashlytics

--- app/src/main/java/sk/vander/electride/App.kt
+++ app/src/main/java/sk/vander/electride/App.kt
@@ -2,13 +2,17 @@ package sk.vander.electride
 
 import android.content.Context
 import android.preference.PreferenceManager
+import android.util.Log
+import com.crashlytics.android.Crashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
 import com.mapbox.mapboxsdk.Mapbox
 import com.patloew.rxlocation.RxLocation
 import dagger.Provides
+import io.fabric.sdk.android.Fabric
 import sk.vander.lib.BaseApp
 import sk.vander.lib.BaseAppModule
 import sk.vander.lib.annotations.ApplicationScope
+import timber.log.Timber
 
 /**
  * @author marian on 5.9.2017.
@@ -22,6 +26,13 @@ abstract class App : BaseApp() {
     Mapbox.getInstance(this, BuildConfig.MAPBOX_TOKEN)
     AndroidThreeTen.init(this)
     PreferenceManager.setDefaultValues(this, R.xml.preferences, true)
+    Fabric.with(this, Crashlytics())
+    Timber.plant(object : Timber.DebugTree() {
+      override fun log(priority: Int, tag: String?, message: String?, t: Throwable?) {
+        if (priority > Log.DEBUG) Crashlytics.log(priority, tag, message)
+        t?.let { Crashlytics.logException(t) }
+      }
+    })
   }
 
   @dagger.Module(includes = arrayOf(BaseAppModule::class))
